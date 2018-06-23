496f393399ae77da4a2c08ad0e63579adc856b3f
Laurynas Butkus <laurynas@baltsoft.com>
2018-06-23 20:00:59 +0300

    Add URL to PDF conversion example

---
+++ examples/convert_url_to_pdf.php
@@ -0,0 +1,29 @@
+<?php
+require __DIR__ . '/../lib/ConvertApi/autoload.php';
+
+use \ConvertApi\ConvertApi;
+
+# set your api secret
+ConvertApi::setApiSecret(getenv('CONVERT_API_SECRET'));
+
+# Example of converting Web Page URL to PDF file
+# https://www.convertapi.com/web-to-pdf
+
+$fromFormat = 'web';
+$conversionTimeout = 180;
+
+$result = ConvertApi::convert(
+    'pdf',
+    [
+        'Url' => 'https://en.wikipedia.org/wiki/Data_conversion',
+        'FileName' => 'web-example'
+    ],
+    $fromFormat,
+    $conversionTimeout
+);
+
+$savedFiles = $result->saveFiles(sys_get_temp_dir());
+
+echo "The web page PDF saved to\n";
+
+print_r($savedFiles);
